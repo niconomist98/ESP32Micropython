70fd91deafa69ea3c4aafde138f4e48540798288
Francisco José Ocampo Giraldo <96587479+fjocampog97@users.noreply.github.com>
2023-03-02 22:06:33 -0500

    punto 2

---
+++ punto_2_a_timesleep_ms.py
@@ -0,0 +1,15 @@
+import dht
+import machine
+import time
+
+d = dht.DHT11(machine.Pin(15))
+
+while True:
+    d.measure()
+    temperatura = d.temperature()
+    humedad = d.humidity()
+
+    print("Temperatura:", temperatura)
+    print("Humedad:", humedad)
+
+    time.sleep_ms(1000)

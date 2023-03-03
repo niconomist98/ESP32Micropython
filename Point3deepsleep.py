da3258a3eaaa71b6cd01d912134ba3a0e86a39bd
Nicolas Restrepo Carvajal <nicorestrepo05@gmail.com>
2023-03-02 22:13:12 -0500

    deepsleep

--- Point3deepsleep.py
+++ Point3deepsleep.py
@@ -3,14 +3,12 @@ import machine
 from machine import Pin
 import time
 
-d = dht.DHT11(Pin(15))
 
-# Using sleep method
+d = dht.DHT11(Pin(15))
+#using deepsleep method
 while True:
     d.measure()
     temp = d.temperature()
     hum = d.humidity()
     print('Temperature: {}°C\tHumidity: {}%'.format(temp, hum))
-    machine.lightsleep(10000)
-    #
-    
+    machine.deepsleep(10000)
